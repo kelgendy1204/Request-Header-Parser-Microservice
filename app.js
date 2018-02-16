af4372859bf0677b9aa0b48daaf74bd4aec5525c
Khaled <elgendy1204@gmail.com>
2018-02-16 23:38:56 +0200

    finish software and language

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const requestIp = require('request-ip');
+var useragent = require('express-useragent');
 
 app.use(requestIp.mw());
+app.use(useragent.express());
 
 app.get('/', (req, res) => {
     res.redirect('/api/whoami');
@@ -10,8 +12,12 @@ app.get('/', (req, res) => {
 
 app.get('/api/whoami', (req, res) => {
     const ipaddress = req.clientIp;
+    const language = req.headers['accept-language'];
+    const software = req.useragent.os;
     const data = {
-        ipaddress
+        ipaddress,
+        language,
+        software
     };
     res.json(data);
 });
